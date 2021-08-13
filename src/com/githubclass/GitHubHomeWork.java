a0872fb05f671662620821f301d2beefcffe127e
tesfaabasmelash <asmelashtesfaab@gmail.com>
2021-08-13 12:58:46 -0500

    The second commit printing 1 to 100.

--- src/com/githubclass/GitHubHomeWork.java
+++ src/com/githubclass/GitHubHomeWork.java
@@ -3,9 +3,11 @@ package com.githubclass;
 public class GitHubHomeWork {
 
 	public static void main(String[] args) {
+		
+		System.out.println("Print integers from 1 to 100");
 
-		for(int i = 1; i <= 10; i++) {
-			System.out.println(i);
+		for(int i = 1; i <= 100; i++) {
+			System.out.print(i + " ");
 		}
 	}
 
