09342ef28616b51142996ccf1f8e82943083aba7
tesfaabasmelash <asmelashtesfaab@gmail.com>
2021-08-13 13:02:54 -0500

    The third commit prints only odd integers from 1 to 100.

--- src/com/githubclass/GitHubHomeWork.java
+++ src/com/githubclass/GitHubHomeWork.java
@@ -4,9 +4,10 @@ public class GitHubHomeWork {
 
 	public static void main(String[] args) {
 		
-		System.out.println("Print integers from 1 to 100");
+		System.out.println("Print Odd integers from 1 to 100");
 
 		for(int i = 1; i <= 100; i++) {
+			if(i % 2 != 0)
 			System.out.print(i + " ");
 		}
 	}
